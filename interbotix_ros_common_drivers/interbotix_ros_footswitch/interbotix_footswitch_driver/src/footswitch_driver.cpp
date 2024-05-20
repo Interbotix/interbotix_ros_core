e889107af140b5167c091dd9a28570716558aada
Luke Schmitt <luke@trossenrobotics.com>
2024-05-20 20:43:46 +0000

    Lint

--- interbotix_ros_common_drivers/interbotix_ros_footswitch/interbotix_footswitch_driver/src/footswitch_driver.cpp
+++ interbotix_ros_common_drivers/interbotix_ros_footswitch/interbotix_footswitch_driver/src/footswitch_driver.cpp
@@ -1,11 +1,34 @@
+// Copyright 2024 Trossen Robotics
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #include <memory>
 
 #include "interbotix_footswitch_driver/footswitch_driver.hpp"
-
-// ACTION!="remove", KERNEL=="event[0-9]*", \
-//    ENV{ID_VENDOR_ID}=="3553", \
-//    ENV{ID_MODEL_ID}=="b001", \
-//    ENV{LIBINPUT_IGNORE_DEVICE}="1"
 
 namespace footswitch_driver
 {
@@ -37,6 +60,11 @@ FootSwitch::FootSwitch(const rclcpp::NodeOptions & options)
   update_period_ms_ = static_cast<int64_t>((1.0 / static_cast<float>(update_rate)) * 1'000);
 
   pub_footswitch_state_ = create_publisher<FootswitchState>("state", 10);
+
+  RCLCPP_INFO(
+    get_logger(),
+    "Publishing footswitch state on topic '%s'.",
+    pub_footswitch_state_->get_topic_name());
 
   tmr_update_state_ = create_wall_timer(
     std::chrono::milliseconds(update_period_ms_),
@@ -83,8 +111,7 @@ void FootSwitch::update_state()
   FootswitchState state;
 
   if (res > 0) {
-    for (int i = 3; i<6; i++) {
-
+    for (int i = 3; i < 6; i++) {
       switch (buf[i]) {
         case 0:
           break;
