249bfae14e4adbce28bf7ec03f7c38b599faa545
Grayson Arendt <grayson.n.arendt5@gmail.com>
2025-01-28 17:08:04 -0600

    Basic formatting on slate node

--- interbotix_ros_slate/interbotix_slate_driver/src/slate_base_node.cpp
+++ interbotix_ros_slate/interbotix_slate_driver/src/slate_base_node.cpp
@@ -33,8 +33,7 @@
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<slate_base::SlateBase>();
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<slate_base::SlateBase>());
   rclcpp::shutdown();
   return 0;
 }
